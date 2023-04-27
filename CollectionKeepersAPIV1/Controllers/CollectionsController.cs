69c186c9b6826f4ca810153afe5dea3ba14f918a
Allan2Fernandes <allan2fernandes@hotmail.com>
2023-04-27 18:09:15 +0200

    Quick fixed a bug with getting random set of collections

--- CollectionKeepersAPIV1/Controllers/CollectionsController.cs
+++ CollectionKeepersAPIV1/Controllers/CollectionsController.cs
@@ -3,6 +3,7 @@ using CollectionKeepersAPIV1.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace CollectionKeepersAPIV1.Controllers
 {
@@ -11,7 +12,7 @@ namespace CollectionKeepersAPIV1.Controllers
     public class CollectionsController : ControllerBase
     {
         CollectionsDbContext ctx;
-        public CollectionsController(CollectionsDbContext context) 
+        public CollectionsController(CollectionsDbContext context)
         {
             ctx = context;
         }
@@ -22,7 +23,7 @@ namespace CollectionKeepersAPIV1.Controllers
             //Get the user with that ID. Make sure the user exists
             List<TblUser> ListOfQueriedUsers = await ctx.TblUsers.Where(User => User.FldUserId == Collection.FldUserId).ToListAsync();
 
-            if(ListOfQueriedUsers.Count == 0)
+            if (ListOfQueriedUsers.Count == 0)
             {
                 return NotFound($"User With ID {Collection.FldUserId} not found");
             }
@@ -43,17 +44,82 @@ namespace CollectionKeepersAPIV1.Controllers
 
             //Query the DB for all the collections for that user
             List<TblCollection> Collections = await ctx.TblCollections.Where(Collection => Collection.FldUserId == InputDetails.FldUserId).ToListAsync();
-
-            List<TblCollection> RandomisedList= new List<TblCollection>();
-
-            for(int i=0; i<InputDetails.NumberRandomCollections; i++)
+            
+            List<TblCollection> RandomisedList = new List<TblCollection>();
+            /*
+            for (int i = 0; i < InputDetails.NumberRandomCollections; i++)
             {
-                TblCollection RandomElement = Collections[rand.Next(Collections.Count)];
+                int randomIndex = rand.Next(Collections.Count);
+                Debug.WriteLine(randomIndex);
+                TblCollection RandomElement = Collections[i];
                 RandomisedList.Add(RandomElement);
                 Collections.Remove(RandomElement);
             }
+            */
+            RandomisedList = Collections.Take(InputDetails.NumberRandomCollections).ToList();
 
-            return Ok(RandomisedList); 
+            return Ok(RandomisedList);
         }
+
+
+        [HttpGet(nameof(GetAnotherUsersPublicCollection) + "/UserID")]
+        public async Task<ActionResult<List<TblCollection>>> GetAnotherUsersPublicCollection(int UserID)
+        {
+            List<TblCollection> Collections = await ctx.TblCollections.Where(Collection => Collection.FldUserId == UserID && (Collection.FldIsPrivate == false)).ToListAsync();
+
+            return Ok(Collections); 
+        }
+
+        //TODO Search all public collections and return a list of collections which match the keyword
+        [HttpGet(nameof(SearchAllPublicCollectionsOnKeyboard) + "/Keyword")]
+        public async Task<ActionResult<List<TblCollection>>> SearchAllPublicCollectionsOnKeyboard(string Keyword)
+        {
+            List<TblCollection> Collections = await ctx.TblCollections.Where(Collection => Collection.FldCollectionName.Contains(Keyword) && (Collection.FldIsPrivate == false)).ToListAsync();
+
+            return Ok(Collections);
+        }
+
+        //Update collection
+        [HttpPut(nameof(UpdateCollection))]
+        public async Task<ActionResult<string>> UpdateCollection(TblCollection CollectionToUpdate)
+        {
+            //Find the correct collection
+            List<TblCollection> ListOfQueriedCollections = await ctx.TblCollections.Where(Collection => CollectionToUpdate.FldCollectionId == Collection.FldCollectionId).ToListAsync();
+
+            if(ListOfQueriedCollections.Count == 0)
+            {
+                return Ok("Collection not found");
+            }
+
+            TblCollection SearchedCollection = ListOfQueriedCollections.First();
+
+            //Update every property of the collection
+            SearchedCollection.FldCollectionName = CollectionToUpdate.FldCollectionName;
+            SearchedCollection.FldCollectionDescription = CollectionToUpdate.FldCollectionDescription;
+            SearchedCollection.FldCollectionThumbnail = CollectionToUpdate.FldCollectionThumbnail;
+            SearchedCollection.FldIsPrivate= CollectionToUpdate.FldIsPrivate;
+
+            return Ok("Collection was updated");
+        }
+
+        //Delete collection
+        [HttpDelete(nameof(DeleteCollection) + "/CollectionID")]
+        public async Task<ActionResult<string>> DeleteCollection(int CollectionID)
+        {
+            List<TblCollection> ListOfQueriedCollections = await ctx.TblCollections.Where(Collection => CollectionID == Collection.FldCollectionId).ToListAsync();
+
+            if(ListOfQueriedCollections.Count == 0)
+            {
+                return Ok("Collection not found");
+            }
+
+            TblCollection FoundCollection = ListOfQueriedCollections.First();
+
+            ctx.TblCollections.Remove(FoundCollection);
+            await ctx.SaveChangesAsync();
+
+            return Ok("Collection successfully deleted");
+        }
+
     }
 }
