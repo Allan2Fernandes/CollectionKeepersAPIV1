dc8e0845a059587420cf51c43095ab0f278c8f3e
Allan2Fernandes <allan2fernandes@hotmail.com>
2023-05-14 14:37:30 +0200

    Added test to check the method which gets attribute values on attribute value IDs

--- XUnitTestProject/DbAttributeValueTests.cs
+++ XUnitTestProject/DbAttributeValueTests.cs
@@ -109,4 +109,58 @@ public class DbAttributeValueTests : IDisposable
             throw;
         }
     }
+    
+    [Fact]
+        public void GetAttributeValuesOnAttributeValueIDTest()
+        {
+            Log.Information("Carrying out test to get attribute on attributeID");
+            var data = new List<TblAttributeValue>
+            {
+                new TblAttributeValue
+                {
+                    FldAttributeValueId = 1,
+                    FldAttributeId = 5,
+                    FldCollectionEntryId = 1,
+                    FldValue = "Nokia"
+                },
+                new TblAttributeValue
+                {
+                    FldAttributeValueId = 2,
+                    FldAttributeId = 5,
+                    FldCollectionEntryId = 1,
+                    FldValue = "Samsung"
+                },
+                new TblAttributeValue
+                {
+                    FldAttributeValueId = 3,
+                    FldAttributeId = 5,
+                    FldCollectionEntryId = 1,
+                    FldValue = "OnePlus"
+                },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<TblAttributeValue>>();
+            mockSet.As<IQueryable<TblAttributeValue>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<TblAttributeValue>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<TblAttributeValue>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<TblAttributeValue>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
+
+            var mockContext = new Mock<CollectionsDbContext>();
+            mockContext.Setup(c => c.TblAttributeValues).Returns(mockSet.Object);
+
+            var service = new AttributeValueServices(mockContext.Object);
+            var QueriedAttributes = service.GetAttributeValueOnID(3);
+
+            try
+            {
+                Assert.Equal(1, QueriedAttributes.Count);
+                Assert.Equal(5, QueriedAttributes.First().FldAttributeId);
+                Assert.Equal("OnePlus", QueriedAttributes.First().FldValue);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                throw;
+            }
+        }
 }
